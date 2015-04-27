10c96173d29ddb0781965f07854a9ac799a06491
Martin Matoušek <martin.matousek@gmail.com>
2015-04-27 23:51:06 +0200

    public

--- src/AclHelper.php
+++ src/AclHelper.php
@@ -2,69 +2,75 @@
 
 namespace V108B\Nette\Security;
 
-abstract class AclHelper {	
+abstract class AclHelper
+{
 	const SIGN_IN_ACTION = ':Sign:in';
 	const ROOT_ROLE = 'root';
 	const ALL = \Nette\Security\Permission::ALL;
-	
-	
-	/** @var Nette\Security\User */
+
+	/** @var \Nette\Security\User */
 	private $user;
-	
+
 	/** @var \Nette\Security\Permission */
-	private $acl;
+	public $acl;
 	
 	public function __construct(\Nette\Security\User $user) {
 		$this->user = $user;
 		$this->acl = new \Nette\Security\Permission();
-		$this->config();	
+		$this->config();
 	}
-	
+
 	public abstract function config();
-	
-	public function addRoles($roles) {		
+
+	public function addRoles($roles)
+	{
 		if ($roles !== self::ALL) {
 			$roles = is_array($roles) ? $roles : [$roles];
-			
-			foreach($roles as $role) {
+
+			foreach ($roles as $role) {
 				if (!$this->acl->hasRole($role)) {
 					$this->acl->addRole($role);
 				}
-			}			
-		}		
+			}
+		}
 	}
-	
-	public function addResources($resources) {
-		if ($resources !== self::ALL ){ 
+
+	public function addResources($resources)
+	{
+		if ($resources !== self::ALL) {
 			$resources = is_array($resources) ? $resources : [$resources];
 
-			foreach($resources as $resource) {
+			foreach ($resources as $resource) {
 				if (!$this->acl->hasResource($resource)) {
 					$this->acl->addResource($resource);
 				}
 			}
 		}
 	}
-	
-	public function allow($roles = self::ALL, $resources = self::ALL, $privileges = self::ALL, $assertion = NULL) {			
+
+	public function allow($roles = self::ALL, $resources = self::ALL, $privileges = self::ALL, $assertion = NULL)
+	{
 		$this->addResources($resources);
 		$this->addRoles($roles);
 		$this->acl->allow($roles, $resources, $privileges, $assertion);
 	}
 
-	public function addRole($role){
+	public function addRole($role)
+	{
 		$this->acl->addRole($role);
 	}
-	
-	public function check($resource, $privilege) {		
+
+	public function check($resource, $privilege)
+	{
 		if ($this->user->isInRole(static::ROOT_ROLE)) {
 			return true;
 		}
-		
-		if (! array_reduce($this->user->getRoles(), function($prev, $role) use ($resource, $privilege) {										
-				return ($this->acl->hasRole($role) && $this->acl->hasResource($resource) && $this->acl->isAllowed($role, $resource, $privilege)) || $prev;
-			}, false)) {
-			throw new \AclException('Unauthorized request :(', 403);				
+
+		if (!array_reduce($this->user->getRoles(), function ($prev, $role) use ($resource, $privilege) {
+			return ($this->acl->hasRole($role) && $this->acl->hasResource($resource) && $this->acl->isAllowed($role, $resource, $privilege)) || $prev;
+		}, false)
+		) {
+			throw new \AclException("Unauthorized access to resource '{$resource}' privilege '{$privilege}' :(", 403);
 		}
-	}	
+	}
 }
